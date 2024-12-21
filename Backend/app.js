9c6fe1d786a820de18425fb0e91bf22a92d53128
Ayushman Mahana <666ayushman@gmail.com>
2024-12-21 18:34:22 +0530

    basic authentication for user completed

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes.js');
 const connectToDb = require('./db/db.js');
 
@@ -12,6 +13,7 @@ connectToDb();
 app.use(cors()); //Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+const userModel = require('../models/user.model.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const blacklistTokenModel = require('../models/blacklistToken.model.js');
+
+module.exports.authUser = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];  // here we are checking if the token is in the cookies or in the headers. and if it is in the headers we are splitting the token from the 'Bearer' keyword. we give question mark before the split method to avoid error if the headers is undefined.
+
+    if (!token) {
+        return res.status(401).json({ message: 'Access Denied' });
+    }
+
+    const isBlacklisted = await blacklistTokenModel.findOne({ token: token });
+
+    if(isBlacklisted) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded._id);
+
+        req.user = user;
+
+        return next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+}
